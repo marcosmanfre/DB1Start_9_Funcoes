8bc7f1cc9eab22e087f919ecdc1611cd5c2fab42
Marcos A. Manfre <marcosmanfre@hotmail.com>
2022-06-15 08:04:49 -0300

    atualizar

--- ex05.py
+++ ex05.py
@@ -1,1 +1,10 @@
 #Escreva um programa que execute uma função que calcule o fatorial de um número passado por parâmetro.
+
+def factorial(n):
+    f = 1
+    for i in range(n, 0, -1):
+        f *= i
+    return f
+
+print(factorial(3))
+
