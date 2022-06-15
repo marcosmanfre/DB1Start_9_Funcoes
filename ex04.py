d2e9c4eef1427cd15f2ceab4238bc984ca4746ca
Marcos A. Manfre <marcosmanfre@hotmail.com>
2022-06-15 08:33:44 -0300

    atualizar

--- ex04.py
+++ ex04.py
@@ -1,1 +1,8 @@
 #Escreva um programa que execute uma função que retorne o inverso de uma string passada por parâmetro.
+
+def inverterTexto(texto):
+        print(texto[::-1])
+   
+ 
+
+inverterTexto('Python é muito bom')
