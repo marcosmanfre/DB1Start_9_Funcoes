6a739a000453930a10773fde5050b084ec553664
Marcos A. Manfre <marcosmanfre@hotmail.com>
2022-06-15 08:21:05 -0300

    atualizar

--- ex08.py
+++ ex08.py
@@ -1,2 +1,23 @@
 #Escreva um programa que execute uma função que receba um número informado no console como parâmetro e verifique se o número informado é primo ou não.
 #Nota: Número primo é um número natural, maior que 1 e que não tenha outros divisores além do 1 e dele mesmo.
+
+
+
+
+def numeroPrimo():
+    num = int(input('Digite um número: '))
+    tot = 0
+    for c in range(1, num + 1):
+        if num % c == 0:
+            print('\033[33m', end=' ')
+            tot += 1
+        else:
+            print('\033[31m', end=' ')
+        print(c, end=' ')
+    print(f'\n\033[mO número {num} foi divisível {tot} vezes')
+    if tot == 2:
+        print('E por isso ele é Primo!')
+    else:
+        print('E por ele não é Primo!')
+
+numeroPrimo()
